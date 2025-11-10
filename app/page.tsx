2f6b34bdd70146e5581a7ed759d716cd7a188ac6
Daniel Kirkpatrick <dankirk15@gmail.com>
2025-11-10 19:57:34 +0000

    Intial website commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Features from "@/components/features-3";
+import HeroSection from "@/components/hero-section";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <HeroSection/>
+      <Features/>
+    </div>
+  );
+}
