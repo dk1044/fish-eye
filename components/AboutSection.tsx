2e2e08ce9172643764a5c2f3aac4a52cf6319133
Daniel Kirkpatrick <dankirk15@gmail.com>
2026-02-03 21:52:56 +0000

    Added a Docker and Next.js logos to AboutSection

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -20,6 +20,36 @@ export default function AboutSection() {
                     <p className="mt-4">This projected was created to help develop my understanding of web development using Next.js,type script and docker, as well as concepts like responsive design, accessibility, and UI/UX principles. It also allowed me to learn about hardware integration and real-time video processing through the use of the ESP32 Camera.</p>
                 </div>
             </div>
+
+                    <div className="mt-8 text-justify"></div>
+                        <Image
+                            src="/pictures/next.js-logo.png"
+                            alt="Next.js Logo"
+                            width={500}
+                            height={500}
+                            className="mb-4 float-none md:float-left md:ml-6"
+                            priority
+                        />
+
+                    <div className="mt-8 text-justify"></div>
+                        <Image
+                            src="/pictures/github.png"
+                            alt="GitHub Logo"
+                            width={500}
+                            height={500}
+                            className="mb-4 float-none md:float-left md:ml-6"
+                            priority
+                        />      
+
+                    <div className="mt-8 text-justify"></div>
+                        <Image
+                            src="/pictures/docker.png"
+                            alt="Docker Logo"
+                            width={500}
+                            height={500}
+                            className="mb-4 float-none md:float-right md:ml-6"
+                            priority
+                        />                                    
         </section>
         </>
     )
